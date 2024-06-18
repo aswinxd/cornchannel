55a4f0681a2ac110e8a248850055f741de232074
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-06-18 20:27:22 +0530

    Update channel.py

--- channel.py
+++ channel.py
@@ -10,8 +10,8 @@ logger = logging.getLogger(__name__)
 
 API_ID = '22710783'
 API_HASH = '616ea341acfed51f916506c20b8a0a44'
-BOT_TOKEN = ''
-MONGO_URI = ""  
+BOT_TOKEN = '7212369479:AAHD0FfwXkzWcax_hjbMJ1xA4VLaI9mKjtg'
+MONGO_URI = "mongodb+srv://test:test@cluster0.q9llhnj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  
 mongo_client = MongoClient(MONGO_URI)
 db = mongo_client['telegram_bot']
 channels_collection = db['channels']
